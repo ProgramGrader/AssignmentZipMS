54226d0258a7c5075d5739f59992151836c49ad3
Zac <43915749+Cliftonz@users.noreply.github.com>
2022-05-22 03:19:40 -0400

    convert doc to pdf

--- terraform/DocumentBucket.tf
+++ terraform/DocumentBucket.tf
@@ -17,6 +17,6 @@ resource "aws_s3_bucket" "url_s3_doc" {
 resource "aws_s3_object" "install_doc" {
   bucket = aws_s3_bucket.url_s3_doc.bucket_domain_name
   key    = "How_to_Install_Jetbrains_Toolbox_and_IDEs"
-  source = "..\\src\\doc_files\\How_to_Install_Jetbrains_Toolbox_and_IDEs.docx"
+  source = "..\\src\\doc_files\\How_to_Install_Jetbrains_Toolbox_and_IDEs.pdf"
 }
 
